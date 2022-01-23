a277a6193facc1608d46eaff0bff5177a5826521
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-22 23:00:40 -0300

    create dark theme

---
+++ src/styles/theme/dark.theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  name: 'dark',
+  colors: {
+    primaryBackground: '#121212',
+    secondaryBackground: '#202020',
+    textColor: 'rgb(255, 255, 255)',
+    textColorDisabled: '#aaa',
+    light: '#ffffff',
+    dark: '#000000',
+    gray: '#3e3e3e',
+    blue: '#3ea6ff',
+  },
+}
