d2e5cdc790f1f15375f5a898f4174dad21b6d999
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-22 23:00:53 -0300

    create light theme

---
+++ src/styles/theme/light.theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  name: 'light',
+  colors: {
+    primaryBackground: '#ffffff',
+    secondaryBackground: '#f8f8f8',
+    textColor: '030303',
+    textColorDisabled: '#aaa',
+    light: '#ffffff',
+    dark: '#000000',
+    gray: '#ccc',
+    blue: '#065fd4',
+  },
+};
