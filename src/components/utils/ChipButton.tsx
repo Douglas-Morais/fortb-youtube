a4de023f41e6f363099293d6215889bde67abbdf
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-23 20:40:56 -0300

    create chip button component

---
+++ src/components/utils/ChipButton.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import styled from "styled-components";
+
+const ChipStyle = styled.button`
+  outline: 0;
+  font-size: 14px;
+  color: ${({theme}) => theme.colors.textColor};
+  background-color: ${({theme}) => theme.colors.gray};
+  padding: 8px 16px;
+  border: .5px solid ${({theme}) => `${theme.colors.light}1a`};
+  border-radius: 20px;
+`;
+
+interface IChip {
+  name: string;
+}
+
+const ChipButton: NextPage<IChip> = (props) => {
+  return (<ChipStyle>{props.name}</ChipStyle>)
+}
+
+export default ChipButton;
