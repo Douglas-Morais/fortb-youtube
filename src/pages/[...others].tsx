17a3a43b153836edd73b0203f0490a3d4fa59233
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-23 13:34:42 -0300

    insert URI information into pages under construction

--- src/pages/[...others].tsx
+++ src/pages/[...others].tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Style = styled.div`
@@ -26,12 +27,16 @@ const Style = styled.div`
   }
 `;
 
-const OthersPage: NextPage = () => {
+
+
+const OthersPage: NextPage = (props) => {
+  const router = useRouter();
+
   return (<Style>
     <Link href="/" shallow={true}>
       <svg focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zm116-292H256v-70.9c0-10.7-13-16.1-20.5-8.5L121.2 247.5c-4.7 4.7-4.7 12.2 0 16.9l114.3 114.9c7.6 7.6 20.5 2.2 20.5-8.5V300h116c6.6 0 12-5.4 12-12v-64c0-6.6-5.4-12-12-12z"></path></svg>
     </Link>
-    <h1>Página em construção...</h1>
+    <h1>{router.asPath} em construção...</h1>
   </Style>)
 }
 
