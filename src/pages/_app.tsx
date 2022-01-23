0ceca22a1f6ea3233bec6ea0e21155045260cc71
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-23 01:02:40 -0300

    change global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
-import { GlobalStyle } from '../styles/global.styles'
+import { GlobalStyle } from '../styles/globals.style'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme } from '../styles/theme/dark.theme'
 import { lightTheme } from '../styles/theme/light.theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (<>
-    <GlobalStyle></GlobalStyle>
+    <GlobalStyle />
     <ThemeProvider theme={darkTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Topnavbar from '../components/layout/Topnavbar'
+import Sidenavbar from '../components/layout/Sidenavbar'
 
 const Home: NextPage = () => {
   return (<>
     <Head>
       <title>Youtube Clone layout</title>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
       <link rel="shortcut icon" href="/icons/logo-youtube.png" type="image/x-icon" />
     </Head>
-    
-    <Topnavbar></Topnavbar>
 
+    <Topnavbar></Topnavbar>
+    <Sidenavbar></Sidenavbar>
   </>)
 }
 
