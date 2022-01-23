b25bb440b5e98f4012c87fe4d1455fd62f2f98c8
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-22 22:57:54 -0300

    override default theme within styled components

---
+++ src/styles/theme/theme.d.ts
@@ -0,0 +1,17 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    name: string;
+    colors: {
+      primaryBackground: string;
+      secondaryBackground: string;
+      textColor: string;
+      textColorDisabled: string;
+      light: string;
+      dark: string;
+      gray: string;
+      blue: string;
+    };
+  }
+}
