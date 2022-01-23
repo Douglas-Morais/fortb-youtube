82dbb73a0563866c7819343692e8363c3fdbff89
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-01-22 23:01:54 -0300

    sets up theme within application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/global.styles'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from '../styles/theme/dark.theme'
+import { lightTheme } from '../styles/theme/light.theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (<>
+    <GlobalStyle></GlobalStyle>
+    <ThemeProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>)
 }
 
-export default MyApp
+export default App
